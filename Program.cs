711584929c52772bb9523c157dbac64736fd10ba
Hunter Becker <epicdroid@outlook.com>
2023-05-12 13:46:23 -0400

    added the ability to search for a specific object instance and extra logging

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class CheckForExistingTimestamps
 {
     static void Main(string[] args)
     {
-        string directoryPath = "C:\\Users\\epicd\\Desktop\\CSVTest\\Importable Data";
+        string directoryPath = "E:\\OneDrive - Elutions Inc\\Documents\\Currently Working On\\NLNG\\NLGN Imprortable 2\\NLGN Imprortable 2\\MAESTRO_IMPORTABLE";
 
         // Get a list of all CSV files in the specified directory
         string[] csvFiles = Directory.GetFiles(directoryPath, "*.csv");
@@ -23,8 +23,12 @@ class CheckForExistingTimestamps
         string startDateString = Console.ReadLine();
         Console.Write("Enter the end date (mm-dd-yyyy or Unix timestamp): ");
         string endDateString = Console.ReadLine();
-        Console.Write("Enter an object instance to query (press enter none): ");
-        string objectId = Console.ReadLine();
+
+        // Prompt user for optional input for the first field ID
+        Console.Write("Enter an optional ID to filter by (leave blank for no filter): ");
+        string optionalIdString = Console.ReadLine();
+        int optionalId;
+        bool filterById = int.TryParse(optionalIdString, out optionalId);
 
         // Parse the start and end dates
         DateTime startDate, endDate;
@@ -51,17 +55,22 @@ class CheckForExistingTimestamps
         }
 
         // Create a log file to write the results
-        string logFilePath = Path.Combine(directoryPath, "search_log.txt");
+        string logFilePath = Path.Combine("D:\\Documents\\SVN\\Projects\\NLGN\\CheckForExistingTimestamps\\CheckForExistingTimestamps", "search_log.log");
         StreamWriter logWriter = new StreamWriter(logFilePath);
 
         // Loop through each CSV file and read each row
         foreach (string csvFile in csvFiles)
         {
+            // Create a set to keep track of unique identifiers we've already found
+            HashSet<string> foundIds = new HashSet<string>();
+            Console.WriteLine("Timestamp not found in file: " + Path.GetFileName(csvFile));
             using (StreamReader reader = new StreamReader(csvFile))
             {
                 string line;
+                bool foundTimestamp = false;
                 while ((line = reader.ReadLine()) != null)
                 {
+
                     // Split the line into its components
                     string[] parts = line.Split(';');
                     if (parts.Length != 5)
@@ -83,11 +92,32 @@ class CheckForExistingTimestamps
                         continue;
                     }
 
-                    // Write the unique identifier and filename to the log file
-                    string logLine = parts[0] + " found in " + csvFile;
-                    logWriter.WriteLine(logLine);
+                    // Check if we've already found this unique identifier
+                    if (foundIds.Contains(parts[0])) //Need to fix this, should add it for the individual file, not just in general
+                    {
+                        continue;
+                    }
 
+                    // Check if we should filter by the first field ID
+                    if (filterById && int.Parse(parts[0]) != optionalId)
+                    {
+                        continue;
+                    }
+
+                    // Write the unique identifier and filename to the log file
+                    string logLine = " Object ID: " + parts[0] + " found in " + Path.GetFileName(csvFile);
+                    logWriter.WriteLine(logLine);
+                    foundTimestamp = true;
                     // Print the unique identifier and filename to the console
+                    Console.WriteLine(logLine);
+
+                    // Add the unique identifier to the set of found IDs
+                    foundIds.Add(parts[0]);
+                }
+                if (foundTimestamp == false)
+                {
+                    string logLine = "Timestamp/object ID not found in file: " + Path.GetFileName(csvFile);
+                    logWriter.WriteLine(logLine);
                     Console.WriteLine(logLine);
                 }
             }
